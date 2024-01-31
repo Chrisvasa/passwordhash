4168920cb853a4771b09d0f554cbd5c0c1ccdfc0
Chrisvasa <Christophervasankari@gmail.com>
2024-01-31 13:26:36 +0100

    Updated Makefile. Added Basic test window to gui.cpp

--- include/gui.h
+++ include/gui.h
@@ -3,7 +3,7 @@
 
 namespace Application
 {
-
+    void RenderUI(void);
 }
 
 #endif
--- src/gui.cpp
+++ src/gui.cpp
@@ -1,6 +1,12 @@
 #include "gui.h"
+#include "imgui.h"
 
 namespace Application
 {
-
+    void RenderUI(void)
+    {
+        ImGui::Begin("Hello");
+        ImGui::Button("Hello");
+        ImGui::End();
+    }
 }
